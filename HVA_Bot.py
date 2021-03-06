93f25248b89674506326a2694b69d1e71993302c
Joost Bakker <78021315+JuicedBakker@users.noreply.github.com>
2021-03-06 10:54:50 +0100

    Update HVA_Bot.py

--- HVA_Bot.py
+++ HVA_Bot.py
@@ -132,10 +132,10 @@ async def biertje(ctx):
 		product = albert.Product(biertjes[i])
 		if product.is_discounted:
 			list += f"- {product.brand} (https://www.ah.nl/producten/product/{biertjes[i]})\n"
-		if len(list) < 1:
-			await ctx.send("Niets is in de aanbieding")  
-		else:
-			await ctx.send(list)
+	if len(list) < 1:
+		await ctx.send("Niets is in de aanbieding")  
+	else:
+		await ctx.send(list)
 
 @bot.event
 async def on_message(message):
